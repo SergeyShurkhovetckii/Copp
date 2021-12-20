12fb33edded07768b0cd899e71652fb3019a371f
S19S93 <S19S93@yandex.ru>
2021-12-20 12:11:22 +0200

    Master

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,14 @@
+import Mmenu from 'mmenu-js';
+
+(function() {
+	new Mmenu( "#menu", {
+		"extensions": [
+		"pagedim-black",
+		"position-front"
+		],
+		"navbar": {
+			"title": "title",
+			"sticky": false
+		},
+	});
+})();
