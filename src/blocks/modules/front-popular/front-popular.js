4c8420923a58f730fe5040d3c47c06b300a0edc1
S19S93 <S19S93@yandex.ru>
2021-12-21 18:08:08 +0200

    Master

--- src/blocks/modules/front-popular/front-popular.js
+++ src/blocks/modules/front-popular/front-popular.js
@@ -1,18 +1,14 @@
 (function () {
     document.addEventListener('DOMContentLoaded', () => {
         let card = document.querySelectorAll('[data-card]');
-        let name = document.querySelectorAll('[data-name]');
 
-        for (let i = 0; i < card.length && i < name.length; i++) {
+        for (let i = 0; i < card.length; i++) {
             card[i].addEventListener('mouseover', () => {
                 card[i].classList.add('popular-cards__card--active');
-                name[i].classList.add('title--white');
 
-                console.log(card[i]);
             });
             card[i].addEventListener('mouseout', () => {
                 card[i].classList.remove('popular-cards__card--active');
-                name[i].classList.remove('title--white');
 
             });
         };
