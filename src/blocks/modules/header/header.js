82b9a45b657dc01e8aa0ecdb9c0af655ea0147ff
S19S93 <S19S93@yandex.ru>
2021-12-22 12:43:46 +0200

    Master

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -20,3 +20,22 @@ import Mmenu from 'mmenu-js';
 		}]
 	});
 })();
+
+
+(function fixedMenuScroll() {
+    const header = document.querySelector('.header');
+	const logo = document.querySelector('.header__logo');
+
+    if (header&&logo) {
+        window.addEventListener('scroll', function () {
+            if (window.scrollY > 0) {
+                header.classList.add('header--fixed');
+				logo.classList.add('header__logo--fixed');
+
+            } else {
+                header.classList.remove('header--fixed');
+				logo.classList.remove('header__logo--fixed');
+            }
+        })
+    }
+})();
