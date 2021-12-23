925fab43aa30b8de8479b0894731096acfa410f6
S19S93 <S19S93@yandex.ru>
2021-12-23 10:48:11 +0200

    Master

--- src/blocks/modules/front-catalog/front-catalog.js
+++ src/blocks/modules/front-catalog/front-catalog.js
@@ -1,13 +1,16 @@
-modules.define('front-catalog', ['i-bem-dom'], function(provide, bemDom) {
+(function () {
+    document.addEventListener('DOMContentLoaded', () => {
+        let catologCard = document.querySelectorAll('[data-catalog]');
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
+        for (let i = 0; i < catologCard.length; i++) {
+            catologCard[i].addEventListener('mouseover', () => {
+                catologCard[i].classList.add('front-catalog__item--active');
 
-});
+            });
+            catologCard[i].addEventListener('mouseout', () => {
+                catologCard[i].classList.remove('front-catalog__item--active');
+
+            });
+        };
+    });
+})();
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import "%modules%/header/header";
 import "%modules%/front-slider/front-slider";
 import "%modules%/front-popular/front-popular";
+import "%modules%/front-catalog/front-catalog";
 import "%modules%/footer/footer"
